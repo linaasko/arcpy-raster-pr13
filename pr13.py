d55b00c5a449f140d6385fe366292ebb36cb26ad
Lina <kokoshalina@gmail.com>
2024-05-19 16:27:08 +0300

    check coordinate systems

--- pr13.py
+++ pr13.py
@@ -4,3 +4,10 @@ raster = arcpy.Raster(r"E:\university\3 course\programming in gis\lections\arcpy
 with arcpy.da.SearchCursor(points,"SHAPE@XY") as cursor:
     for row in cursor:
         print('X:{}, Y:{}'.format(row[0][0], row[0][1]))
+spatial_ref_points = arcpy.Describe(points).spatialReference.name
+spatial_ref_raster = arcpy.Describe(raster).spatialReference.name
+print(spatial_ref_points, spatial_ref_raster)
+if spatial_ref_points == spatial_ref_raster:
+    print('Feature class and raster have the same coordinate systems')
+else:
+    print('Feature class and raster have different coordinate systems')
